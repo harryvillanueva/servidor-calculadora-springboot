f50f427612595ea139ed8bf777162ce2f1e9762b
harryvillanueva <harryvillanueva0814@gmail.com>
2025-10-08 20:18:49 +0200

    actualizacion der cors para que permita angular

--- src/main/java/com/daw/calculadora/config/CorsConfig.java
+++ src/main/java/com/daw/calculadora/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // Aplica a todos los endpoints
 
                 // 1. ORIGEN: Asegúrate de que tu puerto de Vue esté aquí (ej. 5173 o 3000)
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "http://localhost:4200", "http://localhost:3000")
 
                 // 2. MÉTODOS: Incluye POST y OPTIONS (necesario para el pre-flight request)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
